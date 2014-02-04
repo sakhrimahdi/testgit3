d0bf76a198a8cf84840936c8bbff456fdd11d155
sakhri <medaminesakhri@gmail.com>
2014-02-05 00:45:04 +0100

    premier commit

---
+++ testgit2/src/testgit2/sakhri.java
@@ -0,0 +1,13 @@
+package testgit2;
+
+public class sakhri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		
+		System.out.println("c'est mon premier test");
+		
+	}
+
+}
---
+++ testgit2/src/testgit2/tuto.java
@@ -0,0 +1,13 @@
+package testgit2;
+
+public class tuto {
+
+
+
+public void afficher()
+{
+	System.out.println("c'est mon premier test");
+
+}
+
+}
